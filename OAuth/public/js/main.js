d293f909da3b566903ab98b4f0e0fa23de8ad2d3
OneCloud One Economy <onecloud@ucsone.se>
2020-04-01 11:03:15 +0200

    Designed the OAuth login page

--- OAuth/public/js/main.js
+++ OAuth/public/js/main.js
@@ -1,8 +1,10 @@
 window.onload  = () => {
     var redirect_uri = getParameter("redirect_uri");
     if (redirect_uri === null) {
-        document.getElementById('callbackError').style.display = 'block';
-        document.getElementById('loginForm').style.display = 'none';
+        document.getElementById('error').innerHTML = "Redirect URI must be provided.";
+        document.getElementById('errorBox').style.display = "block";
+        document.getElementById('inputs').disabled = true;
+        document.getElementById('inputs').style.opacity = "0.3";
     }
     document.getElementById('redirect_uri').value = redirect_uri;
     getError();
@@ -18,6 +20,7 @@ getError = () => {
     let error = getParameter('error');
     if (error === 'username_password') {
         document.getElementById('error').innerHTML = "Wrong username and/or password, please try again.";
+        document.getElementById('errorBox').style.display = "block";
     }
 }
 
